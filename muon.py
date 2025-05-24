e6f6675747f59b1478fbb180cebc9d2c14979eb1
Keller Jordan <kellerjordan1998@gmail.com>
2025-05-24 13:47:10 -0700

    .

--- muon.py
+++ muon.py
@@ -53,18 +53,14 @@ class Muon(torch.optim.Optimizer):
         nesterov: Whether to use Nesterov-style momentum in the internal SGD. (recommended)
         ns_steps: The number of Newton-Schulz iteration steps to use.
     """
-    def __init__(self, params, lr=0.02, weight_decay=0.01, momentum=0.95, nesterov=True, ns_steps=5, rank=None, world_size=None):
-        if (rank is None) or (world_size is None):
-            raise Exception("world_size and rank params required, if you want to use this optimizer on a single GPU, pass rank=0 and world_size=1.")
-        self.rank = rank
-        self.world_size = world_size
+    def __init__(self, params, lr=0.02, weight_decay=0.01, momentum=0.95, nesterov=True, ns_steps=5):
         defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum, nesterov=nesterov, ns_steps=ns_steps)
         params: list[Tensor] = [*params]
         param_groups = []
         for size in {p.numel() for p in params}:
-            b = torch.empty(world_size, size, dtype=torch.bfloat16, device="cuda")
+            b = torch.empty(dist.get_world_size(), size, dtype=torch.bfloat16, device="cuda")
             group = dict(params=[p for p in params if p.numel() == size],
-                         update_buffer=b, update_buffer_views=[b[i] for i in range(world_size)])
+                         update_buffer=b, update_buffer_views=[b[i] for i in range(dist.get_world_size())])
             param_groups.append(group)
         super().__init__(param_groups, defaults)
 
@@ -83,9 +79,9 @@ class Muon(torch.optim.Optimizer):
                     p_world.mul_(1 - group["lr"] * group["weight_decay"])
                     p_world.add_(g_world.view_as(p_world),
                                  alpha=-group["lr"] * max(1, p_world.size(-2) / p_world.size(-1))**0.5)
-            for base_i in range(len(params))[::self.world_size]:
-                if base_i + self.rank < len(params):
-                    p = params[base_i + self.rank]
+            for base_i in range(len(params))[::dist.get_world_size()]:
+                if base_i + dist.get_rank() < len(params):
+                    p = params[base_i + dist.get_rank()]
                     g = p.grad
                     assert g is not None
                     state = self.state[p]
@@ -98,9 +94,9 @@ class Muon(torch.optim.Optimizer):
                         g = g.view(len(g), -1)
                     g = zeropower_via_newtonschulz5(g, steps=group["ns_steps"]).flatten()
                 else:
-                    g = update_buffer_views[self.rank]
+                    g = update_buffer_views[dist.get_rank()]
                 if base_i > 0:
                     update_prev() # async all_gather instead of sync all_reduce by @YouJiacheng
                 handle = dist.all_gather_into_tensor(update_buffer, g, async_op=True)
-                params_world = params[base_i : base_i + self.world_size]
+                params_world = params[base_i : base_i + dist.get_world_size()]
             update_prev()
