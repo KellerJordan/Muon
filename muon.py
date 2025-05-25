cd9e6c27828844056a8f590d6d99b61f5b9f6389
Keller Jordan <kellerjordan1998@gmail.com>
2025-05-25 13:42:18 -0700

    .

--- muon.py
+++ muon.py
@@ -63,7 +63,7 @@ class Muon(torch.optim.Optimizer):
         lr: The learning rate, in units of spectral norm per update.
         weight_decay: The AdamW-style weight decay.
     """
-    def __init__(self, params, lr=0.02, weight_decay=0.01, momentum=0.95):
+    def __init__(self, params, lr=0.02, weight_decay=0, momentum=0.95):
         defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum)
         assert isinstance(params, list) and len(params) >= 1 and isinstance(params[0], torch.nn.Parameter)
         params = sorted(params, key=lambda x: x.size(), reverse=True)
@@ -90,7 +90,7 @@ class SingleDeviceMuon(torch.optim.Optimizer):
     """
     Muon variant for usage in non-distributed settings.
     """
-    def __init__(self, params, lr=0.02, weight_decay=0.01, momentum=0.95):
+    def __init__(self, params, lr=0.02, weight_decay=0, momentum=0.95):
         defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum)
         super().__init__(params, defaults)
 
